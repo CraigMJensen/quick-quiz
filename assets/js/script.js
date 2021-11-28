cb362db030872ecc2364ede27d10c50261cf3511
CraigJensen <cmjensen82@gmail.com>
2021-11-28 12:38:32 -0600

    tried some things out, didn't work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-/*When I click start quiz the first question appears and the timer begins.
+/*When I click start quiz the first question loads and the timer begins.
 
 *i am told if the answer is correct, add 5 seconds, display next question
 
@@ -15,82 +15,73 @@
 *start button reactivates to start quiz for continued plays */
 
 
-// Array of questions and choices
-// Array of correct answers
-var questions = [{
-    question: "First question",
-    choices: ["Answer a1", "Answer b1", "Answer c1", "Answer d1"],
-    correct: "Correct answer1"
-},
+// Array of questions and choices and correct answers
+var questions = [
+    {
+        question: "First question",
+        choices: ["Answer a1", "Answer b1", "Answer c1", "Answer d1"],
+        answers: "Correct answer1",
+    },
+    {
+        question: "Second question",
+        choices: ["Answer a2", "Answer b2", "Answer c2", "Answer d2"],
+        answers: "Correct answer2",
+    },
+    {
+        question: "Third question",
+        choices: ["Answer a3", "Answer b3", "Answer c3", "Answer d3"],
+        answers: "Correct answer3",
+    },
+    {
+        question: "Fourth question",
+        choices: ["Answer a4", "Answer b4", "Answer c4", "Answer d4"],
+        answers: "Correct answer4",
+    },
+    {
+        question: "Fifth question",
+        choices: ["Answer a5", "Answer b5", "Answer c5", "Answer d5"],
+        answers: "Correct answer5"
+    },
 
-{
-    question: "Second question",
-    choices: ["Answer a1", "Answer b1", "Answer c1", "Answer d1"],
-    correct: "Correct answer2"
-},
-
-{
-    question: "Third question",
-    choices: ["Answer a1", "Answer b1", "Answer c1", "Answer d1"],
-    correct: "Correct answer3"
-},
-
-{
-    question: "Fourth question",
-    choices: ["Answer a1", "Answer b1", "Answer c1", "Answer d1"],
-    correct: "Correct answer4"
-},
-
-{
-    question: "Fifth question",
-    choices: ["Answer a1", "Answer b1", "Answer c1", "Answer d1"],
-    correct: "Correct answer5"
-}];
-
+];
 
 // Variables to call elements
-var body = document.body;
-var container = document.getElementsByClassName('.container');
+
 var quizBox = document.querySelector('#quiz-box');
-var question = document.querySelector('#question');
+var h2El = document.querySelector('#question-text');
 var buttonPress = document.querySelectorAll('.choiceButtons').length;
+
+var questionText = document.querySelector('#question-text');
+var answerA = document.querySelector('.answerA');
+var answerB = document.querySelector('.answerB');
+var answerC = document.querySelector('.answerC');
+var answerD = document.querySelector('.answerD');
+
 var correctAnswer = document.querySelector('#correctAnswer');
+
 var startQuiz = document.querySelector('#start-btn');
+
 var highScore = document.querySelector('#high-score');
 var timer = document.querySelector('#timer');
 var score = document.querySelector('#score');
-var quizText = quizBox.innerHTML;
 
-let currentQuestion = 0;
-
-let q = questions[currentQuestion];
-
+// Create Quiz elements
+var questionIndex = 0;
 
 // Function to beginQuiz
 function beginQuiz() {
     startTimer();
     startQuiz.disabled = true;
-
-    for (var i = 0; i < questions.length; i++) {
-
-
-    }
-
-
+    questionIndex = 0;
 
 
 };
 
-for (var i = 0; i < buttonPress; i++) {
-    document.querySelectorAll(".choiceButtons")[i].addEventListener('click', function () {
+// Next question function
+var nextQuestion = function () {
 
-    })
-};
+}
 
-
-// for (var i = 0; i < questions.length; i++) {
-
-// }
 
 // Function for button clicks to check correct or incorrect answers
 
@@ -108,10 +99,12 @@ function startTimer() {
         } else {
             timer.textContent = 'Timer:';
             clearInterval(timeInterval);
-
-
         }
     }, 1000);
-}
+};
 
 startQuiz.addEventListener('click', beginQuiz);
+// answerA.addEventListener('click',);
+// answerB.addEventListener('click',);
+// answerC.addEventListener('click',);
+// answerD.addEventListener('click',);
