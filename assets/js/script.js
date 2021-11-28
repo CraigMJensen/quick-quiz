59fd6017dee81f256cbe8de8312b65bc8e80fdca
CraigJensen <cmjensen82@gmail.com>
2021-11-28 17:20:05 -0600

    ablility to add first question to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,41 +14,13 @@
 
 *start button reactivates to start quiz for continued plays */
 
-
-// Array of questions and choices and correct answers
-var questions = [
-    {
-        question: "First question",
-        choices: ["Answer a1", "Answer b1", "Answer c1", "Answer d1"],
-        answers: "Correct answer1",
-    },
-    {
-        question: "Second question",
-        choices: ["Answer a2", "Answer b2", "Answer c2", "Answer d2"],
-        answers: "Correct answer2",
-    },
-    {
-        question: "Third question",
-        choices: ["Answer a3", "Answer b3", "Answer c3", "Answer d3"],
-        answers: "Correct answer3",
-    },
-    {
-        question: "Fourth question",
-        choices: ["Answer a4", "Answer b4", "Answer c4", "Answer d4"],
-        answers: "Correct answer4",
-    },
-    {
-        question: "Fifth question",
-        choices: ["Answer a5", "Answer b5", "Answer c5", "Answer d5"],
-        answers: "Correct answer5"
-    },
-
-];
-
 // Variables to call elements
-
-var quizBox = document.querySelector('#quiz-box');
-var h2El = document.querySelector('#question-text');
+var body = document.body;
+var quizBox = document.querySelector('#question-div');
+var answerBtn1 = document.querySelector('#question1');
+var answerBtn2 = document.querySelector('#question2');
+var answerBtn3 = document.querySelector('#question3');
+var answerBtn4 = document.querySelector('#question4');
 var buttonPress = document.querySelectorAll('.choiceButtons').length;
 
 var questionText = document.querySelector('#question-text');
@@ -57,7 +29,7 @@ var answerB = document.querySelector('.answerB');
 var answerC = document.querySelector('.answerC');
 var answerD = document.querySelector('.answerD');
 
-var correctAnswer = document.querySelector('#correctAnswer');
+var showAnswer = document.querySelector('#correctAnswer');
 
 var startQuiz = document.querySelector('#start-btn');
 
@@ -65,15 +37,76 @@ var highScore = document.querySelector('#high-score');
 var timer = document.querySelector('#timer');
 var score = document.querySelector('#score');
 
+
+// Array of questions and choices and correct answers
+var question1 = "What does HTML stand for?";
+var question2 = "What symbol identifies an ID?";
+var question3 = "Which are loop statements in javaScript?";
+var question4 = "How many objects can an array hold?";
+var question5 = "Which of these is NOT javascript object?";
+var questionsArray = [question1, question2, question3, question4, question5];
+
+var choices1 = {
+    answers1: "Hyper Trainer Markup Language",
+    answers2: "Hyper Text Marketing Language",
+    answers3: "Hyper Text Markup Language",
+    answers4: "Hyper Text Marker Leveler"
+};
+var choices2 = {
+    answers1: "*",
+    answers2: "#",
+    answers3: "&",
+    answers4: "!"
+};
+var choices3 = {
+    answers1: "for Statement",
+    answers2: "while Statement",
+    answers3: "for...in Statement",
+    answers4: "All of the above"
+};
+var choices4 = {
+    answers1: "7",
+    answers2: "42",
+    answers3: "88",
+    answers4: "As many as the program calls for"
+};
+var choices5 = {
+    answers1: "Objective",
+    answers2: "Function",
+    answers3: "Boolean",
+    answers4: "Number"
+};
+var choicesArray = [choices1, choices2, choices3, choices4, choices5];
+
+var correctAnswer1 = choices1.answers3;
+var correctAnswer2 = choices2.answers2;
+var correctAnswer3 = choices3.answers4;
+var correctAnswer4 = choices4.answers4;
+var correctAnswer5 = choices5.answers1;
+var correctAnswersArray = [correctAnswer1, correctAnswer2, correctAnswer3, correctAnswer4, correctAnswer5];
+
+
 // Create Quiz elements
-var questionIndex = 0;
+
 
 // Function to beginQuiz
 function beginQuiz() {
     startTimer();
     startQuiz.disabled = true;
-    questionIndex = 0;
 
+    let h2El = document.createElement('h2');
+    h2El.setAttribute('id', 'question-text');
+    h2El.textContent = questionsArray[4];
+    answerBtn1.textContent = (choicesArray[4].answers1);
+    answerBtn2.textContent = (choicesArray[4].answers2);
+    answerBtn3.textContent = (choicesArray[4].answers3);
+    answerBtn4.textContent = (choicesArray[4].answers4);
+    body.appendChild(quizBox);
+    quizBox.appendChild(h2El);
+    quizBox.appendChild(answerBtn1);
+    quizBox.appendChild(answerBtn2);
+    quizBox.appendChild(answerBtn3);
+    quizBox.appendChild(answerBtn4);
 
 };
 
